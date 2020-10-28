a7fc20b589506609716fa10c1edd41b9354b22c0
jtanner <jtanner@sleet.eol.ucar.edu>
2020-10-28 13:43:01 -0600

    parsing time series

--- mysci.py
+++ mysci.py
@@ -1,11 +1,9 @@
 # Read the data file
-#filename = "data/wxobs20170821.txt"
-#datafile = open(filename, 'r')
-#with open(filename, 'r') as datafile:
-#    data = datafile.read()
 
 # Initialize my data variable
-data = []
+data = {'date':[],
+        'time':[],
+        'tempout':[]}
 
 # Read and parse the data file
 filename = "data/wxobs20170821.txt"
@@ -17,6 +15,9 @@ with open(filename, 'r') as datafile:
 
 # Read and parse the rest of the file
     for line in datafile:
-        datum = line.split()
-        data.append(datum)
-
+        split_line = line.split()
+        data['date'].append(split_line[0])
+        data['time'].append(split_line[1])
+        data['tempout'].append(split_line[2])
+#DEBUG
+print(data['time'])
