1e212b3fd4a4414209173322f72b6978c6b3c6e1
jtanner <jtanner@sleet.eol.ucar.edu>
2020-11-11 13:29:02 -0700

    Compute wind chill factor

--- mysci.py
+++ mysci.py
@@ -27,4 +27,24 @@ with open(filename, 'r') as datafile:
             value = t(split_line[i])
             data[column].append(value)
 #DEBUG
-print(data['tempout'])
+#print(data['tempout'])
+
+# Compute the wind chill temperature
+def compute_windchill(t, v):
+    a = 35.74
+    b = 0.6215
+    c = 35.75
+    d = 0.4275
+    
+    v16 = v ** 0.16
+    wci = a + (b * t) - (c * v16) + (d * t * v16)
+    return wci
+
+#Running function to compute wci (windchill)
+windchill = []
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(compute_windchill(temp, windspeed))
+
+
+#DEBUG
+print(windchill)
